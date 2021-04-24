eccae0246ea86cba213809ed08de2502adab21ee
Mahamadi <ouedraogomahamadi218@gmail.com>
2021-04-24 10:35:46 +0000

    push 3

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
   resources :ressource_unite_administratives
   resources :ressource_systeme_irrigues
   resources :ressource_mot_cles
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
   get 'product/trie', to: 'produits#trie'
   get 'langue/recherche/{:code,:nom}', to: 'langues#recherche'
   get 'ressource/recherche/simple/:nom',  to: 'ressources#rechercheSimple'
