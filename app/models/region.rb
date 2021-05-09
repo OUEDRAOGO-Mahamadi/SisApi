e0a737f90cdbf4c07b87483a075970d45eb1d484
Mahamadi <ouedraogomahamadi218@gmail.com>
2021-05-09 18:41:44 +0000

    push 05

--- app/models/region.rb
+++ app/models/region.rb
@@ -5,6 +5,9 @@ class Region < ApplicationRecord
     has_many:region_localites
     has_many :localites, :through => :region_localites,dependent: :delete_all
 
+    has_many:pays_regions
+    has_many :pays, :through => :pays_regions,dependent: :delete_all
+
     accepts_nested_attributes_for:region_localites,allow_destroy:true
   
 end
--- db/migrate/20210503181848_create_pays_regions.rb
+++ db/migrate/20210503181848_create_pays_regions.rb
@@ -1,7 +1,7 @@
 class CreatePaysRegions < ActiveRecord::Migration[6.1]
   def change
     create_table :pays_regions do |t|
-      t.references :pays, null: false, foreign_key: true
+      t.references :pay, null: false, foreign_key: true
       t.references :region, null: false, foreign_key: true
 
       t.timestamps
