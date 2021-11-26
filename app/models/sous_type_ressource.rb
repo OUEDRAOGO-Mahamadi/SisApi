88ae85ce873bb8183754f0af160ca754fbd84fa1
OUEDRAOGO-Mahamadi <ouedraogomahamadi218@gmail.com>
2021-11-26 11:10:02 +0000

    ok

--- app/models/sous_type_ressource.rb
+++ app/models/sous_type_ressource.rb
@@ -1,11 +1,11 @@
 class SousTypeRessource < ApplicationRecord
-    has_many:ressources
+   # has_many:ressources
     has_many:type_sous_type_ressources
     has_many:ressource_sous_ressources
 
-    # has_many :type_ressources, :through => :ressource_sous_ressource,dependent: 
+    has_many :ressources, :through => :ressource_sous_ressources
     has_many :type_ressources, :through => :type_sous_type_ressources,dependent: :delete_all
 
     accepts_nested_attributes_for:type_sous_type_ressources,allow_destroy:true
-    accepts_nested_attributes_for:ressource_sous_ressources,allow_destroy:true
+   # accepts_nested_attributes_for:ressource_sous_ressources,allow_destroy:true
 end
