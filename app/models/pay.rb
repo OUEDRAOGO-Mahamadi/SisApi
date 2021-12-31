6a35e83243ccf59f30289da581a9c3b24ad24641
OUEDRAOGO-Mahamadi <ouedraogomahamadi218@gmail.com>
2021-12-31 06:26:44 +0000

    ok

--- app/models/pay.rb
+++ app/models/pay.rb
@@ -1,12 +1,15 @@
 class Pay < ApplicationRecord
     has_many:pays_regions
-    
+    has_many:utilisateur_pays
     has_many:ressource_pays
     has_many:pays_unite_administratives
+    
+    has_many :utilisateurs, :through => :utilisateur_pays,dependent: :delete_all
     has_many :unite_administratives, :through => :pays_unite_administratives,dependent: :delete_all
     has_many :regions, :through => :pays_regions,dependent: :delete_all
     has_many :ressources, :through => :ressource_pays,dependent: :delete_all
 
+    accepts_nested_attributes_for:utilisateur_pays,allow_destroy:true
     accepts_nested_attributes_for:pays_regions,allow_destroy:true
     accepts_nested_attributes_for:regions
     accepts_nested_attributes_for:pays_unite_administratives,allow_destroy:true
