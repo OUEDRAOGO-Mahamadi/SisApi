0b6561fb4e70187ca8f310e37c6fcc40539a794c
OUEDRAOGO-Mahamadi <ouedraogomahamadi218@gmail.com>
2022-02-07 12:10:53 +0000

    ok

--- app/models/type_expert.rb
+++ app/models/type_expert.rb
@@ -1,12 +1,12 @@
 class TypeExpert < ApplicationRecord
     
-    has_many:type_expert_sous
+    has_many:type_sous_experts
     
     
    
-    has_many :sous_type_experts, :through => :type_expert_sous,dependent: :delete_all
+    has_many :sous_type_experts, :through => :type_sous_experts,dependent: :delete_all
 
-    accepts_nested_attributes_for:type_expert_sous,allow_destroy:true
+    accepts_nested_attributes_for:type_sous_experts,allow_destroy:true
    
   
 end
